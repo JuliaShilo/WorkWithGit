71c429c7f12ca9abfbb7efb5908c18f0dc930b15
JuliaShilo <akira677@mail.ru>
2019-05-23 19:41:34 +0300

    Initial commit

---
+++ src/org/itstep/ApplicationRunner.java
@@ -0,0 +1,13 @@
+package org.itstep;
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        someMethod("Hi! My name is Yulia");
+
+    }
+
+    static void someMethod(String msg) {
+        System.out.println(msg);
+    }
+
+}
